3484de7a2a3b44001cc4c6401038670c9243234f
Yue Gao <hologerry@gmail.com>
2024-11-02 17:21:36 +0800

    update

--- sat/finetune_i2v_multi_gpus_red.sh
+++ sat/finetune_i2v_multi_gpus_red.sh
@@ -2,7 +2,7 @@
 
 echo "RUN on $(hostname)"
 
-run_cmd="torchrun --standalone --nproc_per_node=4 train_image_to_video.py --base configs/cogvideox_5b_lora_i2v.yaml configs/sft_i2v_realcapturered.yaml --seed $RANDOM"
+run_cmd="torchrun --standalone --nproc_per_node=4 train_image_to_video.py --base configs/cogvideox_5b_lora_i2v.yaml configs/sft_i2v_red.yaml --seed $RANDOM"
 
 echo ${run_cmd}
 eval ${run_cmd}
---
+++ sat/finetune_multi_gpus_blue.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+echo "RUN on $(hostname)"
+
+run_cmd="torchrun --standalone --nproc_per_node=4 train_video.py --base configs/cogvideox_5b_lora.yaml configs/sft_blue.yaml --seed $RANDOM"
+
+# run_cmd="PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True torchrun --standalone --nproc_per_node=8 train_video.py --base configs/test_cogvideox_5b_i2v_lora.yaml configs/test_sft.yaml --seed $RANDOM"
+
+echo ${run_cmd}
+eval ${run_cmd}
+
+echo "DONE on `hostname`"
