8ebeaeecea5b756b5f84bd6ad64dd89bd0e93568
Yue Gao <hologerry@gmail.com>
2024-11-08 13:19:50 +0800

    update

---
+++ tools_sdedit/sdedit_video_prefixi2v_future_wind_blue.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+{
+# echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+
+environs="WORLD_SIZE=1 RANK=0 LOCAL_RANK=0 LOCAL_WORLD_SIZE=1"
+
+run_cmd="$environs python sdedit_video_pi2v_future.py --base configs/cogvideox_5b_lora_prefixi2v.yaml configs_sdedit/sdedit_zero123_prefixi2v_future_wind_blue.yaml --seed $RANDOM"
+echo ${run_cmd}
+eval ${run_cmd}
+
+
+echo "DONE on `hostname`"
+exit
+}
