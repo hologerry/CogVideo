afdf123f20c6714bbdff3bb4d054caa1445533bb
Yue Gao <hologerry@gmail.com>
2024-11-13 20:50:54 +0800

    update

--- tools_sdedit/sdedit_video_zero123_pi2v_ott_scalar.sh
+++ tools_sdedit/sdedit_video_zero123_pi2v_ott_scalar.sh
@@ -4,7 +4,7 @@
 
 environs="WORLD_SIZE=1 RANK=0 LOCAL_RANK=0 LOCAL_WORLD_SIZE=1"
 
-run_cmd="$environs python sdedit_video_zero123_pi2v_onetwothree.py --base configs/cogvideox_5b_lora_prefixi2v.yaml configs_sdedit/sdedit_zero123_prefixi2v_scalar.yaml --seed $RANDOM"
+run_cmd="$environs python sdedit_video_zero123_pi2v_onetwothree.py --base configs/cogvideox_5b_lora_prefixi2v.yaml configs_sdedit/sdedit_zero123_pi2v_ott_scalar.yaml --seed $RANDOM"
 echo ${run_cmd}
 eval ${run_cmd}
 
